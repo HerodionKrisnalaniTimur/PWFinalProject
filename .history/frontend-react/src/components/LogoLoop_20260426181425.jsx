ae476b8fc6afd6b3454bb3863e341430cccd32ee
bagasskoro <prasetiobagaskoro@students.amikom.ac.id>
2026-07-16 18:18:19 +0700

    Delete .history directory

--- .history/frontend-react/src/components/LogoLoop_20260426181425.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LogoLoop = () => {
-  return (
-    <div>LogoLoop</div>
-  )
-}
-
-export default LogoLoop
--- .history/frontend-react/src/components/LogoLoop_20260426181453.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LogoLoop from "../compoonents/LogoLoop"; 
-const LogoLoop = () => {
-  return (
-    <div>LogoLoop</div>
-  )
-}
-
-export default LogoLoop
